f81745dc9ff2b5e3463085ebeac83972eb187a07
Omar Bakr <omarbakr301@gmail.com>
2021-07-11 05:33:30 +0200

    Implemented Stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { login, logout } from "./login";
 import { signup } from './signup';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -75,5 +76,8 @@ if(bookBtn) {
     e.target.textContent = 'Processing...'
     const { tourId } = e.target.dataset;
     bookTour(tourId);
-  })
+  });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
