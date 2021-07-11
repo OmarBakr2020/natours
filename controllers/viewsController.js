19c391b7579cfa984dad40e9648a5e6c16a7a629
Omar Bakr <omarbakr301@gmail.com>
2021-07-11 15:22:47 +0200

    Updated viewsController file

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,8 +8,9 @@ exports.alerts = (req, res, next) => {
     const {alert} = req.query;
     if(alert === 'booking') {
         res.locals.alert = 'Your booking was successful! Please check your email for a confirmation. If your booking doesn\'t show up immediately, please come back later,';
-        next();
+        
     }
+    next();
 }
 
 exports.getOverview = catchAsync(async (req, res, next) => {
